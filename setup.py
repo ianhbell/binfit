390e23cfdd805b02d0c8711dced8ec21f00ca40e
Ian Bell <ian.bell@nist.gov>
2019-09-12 17:55:09 -0600

    Make packageable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='binfit',
+      version='0.0',
+      description='A python tool for fitting binary interaction parameters for GERG-like models',
+      author='Ian H. Bell',
+      author_email='ian.bell@nist.gov',
+      packages = ['.'],
+      zip_safe = False
+     )
